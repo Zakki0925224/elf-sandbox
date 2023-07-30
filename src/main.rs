c3186fb0b9c024259d1fa106f6f199ababbb389c
Zakki0925224 <zakki0925224@gmail.com>
2023-07-30 16:10:08 +0900

    [Add]: Added lxc test code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-mod lxc;
+use sudo::RunningAs;
 
 fn main() {
-    lxc::exec_cmd(&[
-        "launch",
-        "images:ubuntu/22.04",
-        "ubuntu-sandbox",
-        "--vm",
-        "-c",
-        "security.secureboot=false",
-    ]);
+    match sudo::check() {
+        RunningAs::Root => (),
+        _ => panic!("You must be run as sudo"),
+    }
 
-    lxc::exec_cmd(&["shell", "ubuntu-sandbox"]);
-    lxc::exec_cmd(&["stop", "ubuntu-sandbox"]);
-    lxc::exec_cmd(&["delete", "ubuntu-sandbox"]);
-    lxc::exec_cmd(&["list"]);
+    let container_name = "sandbox";
+    let distoribution = "ubuntu";
+    let release = "bionic";
+    let arch = "amd64";
+
+    let c =
+        lxc::Container::new(container_name, None).expect("Failed to setup lxc_container struct");
+
+    if c.is_defined() {
+        panic!("Container already exists");
+    }
+
+    println!("Creating container...");
+    c.create(
+        "download",
+        None,
+        None,
+        ::lxc::CreateFlags::QUIET,
+        &["-d", distoribution, "-r", release, "-a", arch],
+    )
+    .expect("Failed to create container rootfs");
+
+    println!("Starting container...");
+    c.start(false, &[]).expect("Failed to start the container");
+
+    println!("Stopping container...");
+    c.stop().expect("Failed to kill the container.");
+
+    println!("Destoroying container...");
+    c.destroy().expect("Failed to destroy the container.");
 }
